d9fdbfbd2d075da116d619e07d328cf673d1bd1a
Abrham <abrsh6266@gmail.com>
2024-04-08 02:22:18 -0800

    configured login page

---
+++ src/components/SubmitBtn.jsx
@@ -0,0 +1,24 @@
+import { useNavigation } from "react-router-dom";
+
+const SubmitBtn = ({ text }) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state == "submitting";
+
+  return (
+    <button
+      className="btn btn-primary btn-block "
+      disabled={isSubmitting}
+      type="submit"
+    >
+      {isSubmitting ? (
+        <>
+          <span className="loading loading-spinner">Sending...</span>
+        </>
+      ) : (
+        text
+      )}
+    </button>
+  );
+};
+
+export default SubmitBtn;
