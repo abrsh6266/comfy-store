c17cac1dd398f0024514280a94af3dabd3b133c2
Abrham <abrsh6266@gmail.com>
2024-04-20 11:30:20 -0800

    range input configured

--- src/components/FormRange.jsx
+++ src/components/FormRange.jsx
@@ -1,5 +1,34 @@
-const FormRange = ({ label, type, size }) => {
-  return <div>FormRange</div>;
+import { useState } from "react";
+import { formatPrice } from "../utils";
+
+const FormRange = ({ label, name, size }) => {
+  const step = 1000;
+  const maxPrice = 100000;
+  const [selectedPrice, setSelectedPrice] = useState(maxPrice);
+  return (
+    <div className=" form-control">
+      <label htmlFor={name} className="label cursor-pointer">
+        <span className="label-text capitalize">{label}</span>
+        <span>{formatPrice(selectedPrice)}</span>
+      </label>
+      <input
+        type="range"
+        name={name}
+        min={0}
+        max={maxPrice}
+        value={selectedPrice}
+        onChange={(e) => {
+          setSelectedPrice(e.target.value);
+        }}
+        step={step}
+        className={`range range-primary ${size}`}
+      />
+      <div className="w-full flex justify-between text-xs px-2 mt-2">
+        <span className="font-bold text-dm">0</span>
+        <span className="font-bold text-dm">Max: {formatPrice(maxPrice)}</span>
+      </div>
+    </div>
+  );
 };
 
 export default FormRange;
