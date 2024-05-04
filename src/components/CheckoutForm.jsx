9c36af8ce30e4a814328c3806c91144c60d21935
Abrham <abrsh6266@gmail.com>
2024-05-04 02:54:06 -0800

    auth error configured

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,9 +1,9 @@
-import { Form, redirect } from 'react-router-dom';
-import FormInput from './FormInput';
-import SubmitBtn from './SubmitBtn';
-import { customFetch, formatPrice } from '../utils';
-import { toast } from 'react-toastify';
-import { clearCart } from '../features/cart/cartSlice';
+import { Form, redirect } from "react-router-dom";
+import FormInput from "./FormInput";
+import SubmitBtn from "./SubmitBtn";
+import { customFetch, formatPrice } from "../utils";
+import { toast } from "react-toastify";
+import { clearCart } from "../features/cart/cartSlice";
 
 export const action =
   (store) =>
@@ -24,7 +24,7 @@ export const action =
     };
     try {
       const response = await customFetch.post(
-        '/orders',
+        "/orders",
         { data: info },
         {
           headers: {
@@ -33,26 +33,27 @@ export const action =
         }
       );
       store.dispatch(clearCart());
-      toast.success('order placed successfully');
-      return redirect('/orders');
+      toast.success("order placed successfully");
+      return redirect("/orders");
     } catch (error) {
       console.log(error);
       const errorMessage =
         error?.response?.data?.error?.message ||
-        'there was an error placing your order';
-
+        "there was an error placing your order";
       toast.error(errorMessage);
+      if (error?.response?.status === 401 || 403) return redirect("/login");
+
       return null;
     }
   };
 const CheckoutForm = () => {
   return (
-    <Form method='POST' className='flex flex-col gap-y-4'>
-      <h4 className='font-medium text-xl'>Shipping Information</h4>
-      <FormInput label='first name' name='name' type='text' />
-      <FormInput label='address' name='address' type='text' />
-      <div className='mt-4'>
-        <SubmitBtn text='Place Your Order' />
+    <Form method="POST" className="flex flex-col gap-y-4">
+      <h4 className="font-medium text-xl">Shipping Information</h4>
+      <FormInput label="first name" name="name" type="text" />
+      <FormInput label="address" name="address" type="text" />
+      <div className="mt-4">
+        <SubmitBtn text="Place Your Order" />
       </div>
     </Form>
   );
