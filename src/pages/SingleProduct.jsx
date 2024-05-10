0ed0599699b976d2ed990963c2f0f1cbc6afe533
Abrham <abrsh6266@gmail.com>
2024-05-10 02:54:46 -0800

    setup react query

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addItem } from "../features/cart/cartSlice";
-export const loader = async ({ params }) => {
+export const loader =(queryClient)=> async ({ params }) => {
   const response = await customFetch(`/products/${params.id}`);
   return { product: response.data.data };
 };
